0e7c6f0f2801b47f242a7d58b83e406835ab355e
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-29 20:24:55 +0530

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token)
+    return res.status(401).json({ message: "Access denied. No token provided." });
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified; // contains id, role
+    next();
+  } catch (err) {
+    res.status(400).json({ message: "Invalid token" });
+  }
+};
+
+export const adminOnly = (req, res, next) => {
+  if (req.user.role !== "admin")
+    return res.status(403).json({ message: "Admin only" });
+
+  next();
+};
